214e9d088455126620e414cc2fd667f46a1b9d85
ZongHaoDu <113969717+ZongHaoDu@users.noreply.github.com>
2022-09-23 21:09:17 +0800

    test2

---
+++ Contents/section1/test2.cpp
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+#include<bits/stdc++.h>
+using namespace std;
+int main{
+    
+}
+=======
+#include<bit/stdc++.h>
+using namespace std
+int main{
+    
+}
+>>>>>>> 6fc4977025739b04c872c579369668c5af4a6fe4
